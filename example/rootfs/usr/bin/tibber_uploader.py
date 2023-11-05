ab4ea39bd9347e8d996acee63d14103ab03ffa1f
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-05 22:45:31 +0100

    Update tibber_uploader.py

--- example/rootfs/usr/bin/tibber_uploader.py
+++ example/rootfs/usr/bin/tibber_uploader.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 
 import logging
-import aiohttp
-from homeassistant.core import HomeAssistant
+import os
+import requests
+from datetime import datetime
 
 _LOGGER = logging.getLogger(__name__)
 
 class TibberUploader:
-    def __init__(self, hass: HomeAssistant, token: str, meter_id: str, register_id: str, meter_sensor: str):
-        self.hass = hass
+    def __init__(self, token: str, meter_id: str, register_id: str, meter_sensor: str):
         self.token = token
         self.meter_id = meter_id
         self.register_id = register_id
         self.meter_sensor = meter_sensor
+        self.supervisor_token = os.getenv('SUPERVISOR_TOKEN')
 
-    async def upload_reading(self, reading: int):
+    def upload_reading(self, reading: int):
         """Upload the meter reading to Tibber."""
-        url = "https://app.tibber.com/v4/gql"
+        # Hier verwenden wir die Supervisor API, um die aktuelle Zeit von Home Assistant zu erhalten
+        hass_url = "http://supervisor/core/api/states/sensor.date__time"
         headers = {
+            "Authorization": f"Bearer {self.supervisor_token}",
+            "Content-Type": "application/json",
+        }
+        response = requests.get(hass_url, headers=headers)
+        if response.status_code == 200:
+            reading_date = response.json()['state']
+        else:
+            _LOGGER.error("Failed to get current time from Home Assistant")
+            return
+
+        # Jetzt führen wir die Mutation auf der Tibber API durch
+        tibber_url = "https://api.tibber.com/v1-beta/gql"
+        tibber_headers = {
             "Authorization": f"Bearer {self.token}",
             "Content-Type": "application/json",
         }
-        data = {
+        tibber_data = {
             "query": """
                 mutation AddMeterReadings($meterId: ID!, $readingDate: String!, $registerId: String!, $value: Int!) {
                     me {
@@ -53,15 +68,14 @@ class TibberUploader:
             """,
             "variables": {
                 "meterId": self.meter_id,
-                "readingDate": self.hass.helpers.dt.now().isoformat(),
+                "readingDate": reading_date,
                 "registerId": self.register_id,
                 "value": reading,
             },
         }
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=headers, json=data) as response:
-                if response.status == 200:
-                    _LOGGER.info("Meter reading uploaded successfully")
-                else:
-                    _LOGGER.error("Failed to upload meter reading: %s", response.status)
+        tibber_response = requests.post(tibber_url, headers=tibber_headers, json=tibber_data)
+        if tibber_response.status_code == 200:
+            _LOGGER.info("Meter reading uploaded successfully")
+        else:
+            _LOGGER.error("Failed to upload meter reading: %s", tibber_response.status_code)
