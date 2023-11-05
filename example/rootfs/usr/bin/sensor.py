e3acf77c2441663bee506756f6f73b1f1af3b247
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-05 22:46:33 +0100

    Update sensor.py

--- example/rootfs/usr/bin/sensor.py
+++ example/rootfs/usr/bin/sensor.py
@@ -1,32 +1,32 @@
 import logging
+import os
+import requests
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_TOKEN
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, CONF_METER_SENSOR
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+async def async_setup_entry(hass, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     """Set up the Tibber Reading Uploader sensor from a config entry."""
     token = entry.data.get(CONF_TOKEN)
     meter_sensor = entry.data.get(CONF_METER_SENSOR)
 
     if token and meter_sensor:
-        async_add_entities([TibberReadingUploaderSensor(hass, token, meter_sensor)])
+        async_add_entities([TibberReadingUploaderSensor(token, meter_sensor)])
 
 class TibberReadingUploaderSensor(SensorEntity):
     """Representation of a Tibber Reading Uploader sensor."""
 
-    def __init__(self, hass: HomeAssistant, token: str, meter_sensor: str):
+    def __init__(self, token: str, meter_sensor: str):
         """Initialize the sensor."""
-        self._hass = hass
         self._token = token
         self._meter_sensor = meter_sensor
         self._state = None
+        self.supervisor_token = os.getenv('SUPERVISOR_TOKEN')
 
     @property
     def name(self):
@@ -40,13 +40,20 @@ class TibberReadingUploaderSensor(SensorEntity):
 
     async def async_update(self):
         """Fetch new state data for the sensor."""
-        # Get the meter reading from the selected sensor
-        meter_sensor_state = self._hass.states.get(self._meter_sensor)
-        if meter_sensor_state is None:
-            _LOGGER.error("Meter sensor not found: %s", self._meter_sensor)
+        # Get the meter reading from the selected sensor using Supervisor API
+        hass_url = f"http://supervisor/core/api/states/{self._meter_sensor}"
+        headers = {
+            "Authorization": f"Bearer {self.supervisor_token}",
+            "Content-Type": "application/json",
+        }
+        response = requests.get(hass_url, headers=headers)
+        if response.status_code == 200:
+            meter_sensor_state = response.json()
+        else:
+            _LOGGER.error("Failed to get meter sensor state: %s", response.status_code)
             return
 
-        meter_reading = meter_sensor_state.state
+        meter_reading = meter_sensor_state['state']
         if meter_reading is None:
             _LOGGER.error("Meter reading is None")
             return
@@ -59,5 +66,5 @@ class TibberReadingUploaderSensor(SensorEntity):
             return
 
         # Send the meter reading to Tibber
-        uploader = self._hass.data[DOMAIN]
-        await uploader.upload_reading(meter_reading)
+        uploader = TibberUploader(self._token, meter_sensor_state['attributes']['meter_id'], meter_sensor_state['attributes']['register_id'], self._meter_sensor)
+        uploader.upload_reading(meter_reading)
