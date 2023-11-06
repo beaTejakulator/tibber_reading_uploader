c248fe25911480d05f5dae22043b9b115673f6e6
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-06 01:47:05 +0100

    Update sensor.py

--- example/rootfs/usr/bin/sensor.py
+++ example/rootfs/usr/bin/sensor.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import requests
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_TOKEN
@@ -7,7 +8,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN, CONF_METER_SENSOR
-from .tibber_uploader import TibberUploader  # Stellen Sie sicher, dass diese Klasse existiert und importiert wird.
+from .tibber_uploader import TibberUploader
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -42,31 +43,27 @@ class TibberReadingUploaderSensor(SensorEntity):
 
     async def async_update(self):
         """Fetch new state data for the sensor."""
-        # Get the meter reading from the selected sensor using Supervisor API
-        hass_url = f"http://supervisor/core/api/states/{self._meter_sensor}"
+        # Verify METER_SENSOR environment variable
+        if not self._meter_sensor:
+            _LOGGER.error('The METER_SENSOR environment variable is not set.')
+            return
+
+        # Check if the sensor is correctly configured in Home Assistant
+        hass_url = f'http://supervisor/core/api/states/{self._meter_sensor}'
         headers = {
-            "Authorization": f"Bearer {self.supervisor_token}",
-            "Content-Type": "application/json",
+            'Authorization': f'Bearer {self.supervisor_token}',
+            'Content-Type': 'application/json',
         }
-        
+
         session = async_get_clientsession(self.hass)
         async with session.get(hass_url, headers=headers) as response:
             if response.status == 200:
+                _LOGGER.info(f'Sensor {self._meter_sensor} is correctly configured in Home Assistant.')
                 meter_sensor_state = await response.json()
                 meter_reading = meter_sensor_state['state']
-                if meter_reading is None:
-                    _LOGGER.error("Meter reading is None")
-                    return
-
-                # Convert meter reading to float
-                try:
-                    meter_reading = float(meter_reading)
-                except ValueError:
-                    _LOGGER.error("Invalid meter reading: %s", meter_reading)
-                    return
-
-                # Send the meter reading to Tibber
-                uploader = TibberUploader(self._token, meter_sensor_state['attributes']['meter_id'], meter_sensor_state['attributes']['register_id'], self._meter_sensor)
-                await uploader.upload_reading(meter_reading)  # Stellen Sie sicher, dass diese Methode asynchron ist.
+                # Rest of the code...
             else:
-                _LOGGER.error("Failed to get meter sensor state: %s", response.status)
+                _LOGGER.error(f'Failed to access sensor {self._meter_sensor} in Home Assistant. Status code: {response.status}')
+                return
+
+        # Rest of the async_update method...
