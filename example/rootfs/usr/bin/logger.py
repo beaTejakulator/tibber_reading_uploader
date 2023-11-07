c9c1bbb97e62a34ddb357ee3aada9790c50d72e7
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-07 18:52:00 +0100

    Create logger.py

---
+++ example/rootfs/usr/bin/logger.py
@@ -0,0 +1,12 @@
+# logger.py
+import logging
+import sys
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(sys.stdout)  # Log-Ausgabe auf stdout
+        ]
+    )
