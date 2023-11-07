c67876e59184a5668dfe6988d363b978dc7d0150
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-07 23:36:07 +0100

    Update main.py

--- example/rootfs/usr/bin/main.py
+++ example/rootfs/usr/bin/main.py
@@ -1,6 +1,11 @@
 import os
 from auth import get_tibber_token
 from uploader import TibberUploader
+import logging
+
+# Konfigurieren Sie das Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     # Anmeldeinformationen aus Umgebungsvariablen lesen
@@ -9,10 +14,16 @@ if __name__ == "__main__":
 
     # Überprüfen, ob Anmeldeinformationen vorhanden sind
     if not email or not password:
+        logger.error("Die Anmeldeinformationen für Tibber sind nicht gesetzt.")
         raise ValueError("Die Anmeldeinformationen für Tibber sind nicht gesetzt.")
 
     # Token abrufen
-    token = get_tibber_token(email, password)
+    try:
+        token = get_tibber_token(email, password)
+        logger.info("Token erfolgreich abgerufen.")
+    except Exception as e:
+        logger.error(f"Fehler beim Abrufen des Tokens: {e}")
+        raise
 
     # Umgebungsvariablen für die restlichen Parameter lesen
     meter_id = os.getenv('METER_ID')
@@ -20,5 +31,11 @@ if __name__ == "__main__":
     meter_sensor = os.getenv('METER_SENSOR')
 
     # TibberUploader-Instanz erstellen und Ausführung starten
-    uploader = TibberUploader(token, meter_id, register_id, meter_sensor)
-    uploader.upload_reading()
+    try:
+        uploader = TibberUploader(token, meter_id, register_id, meter_sensor)
+        uploader.upload_reading()
+        logger.info("Upload-Prozess erfolgreich gestartet.")
+    except Exception as e:
+        logger.error(f"Fehler beim Starten des Upload-Prozesses: {e}")
+        raise
+
