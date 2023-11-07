23c522187f91ee9ba6387b598652d33a640f5a1f
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-07 18:57:44 +0100

    Update logger.py

--- example/rootfs/usr/bin/logger.py
+++ example/rootfs/usr/bin/logger.py
@@ -1,8 +1,8 @@
-# logger.py
 import logging
 import sys
 
 def setup_logging():
+    # Konfigurieren Sie das Logging, um auf die Standardausgabe zu schreiben
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
