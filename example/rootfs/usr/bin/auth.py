d7f69cbb51487a5eda88384b54ad11284aefd5ec
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2023-11-07 22:26:05 +0100

    Update auth.py

--- example/rootfs/usr/bin/auth.py
+++ example/rootfs/usr/bin/auth.py
@@ -1,10 +1,4 @@
-import logging  # Diesen Import hinzufügen
-
-from logger import setup_logging
-setup_logging()
-
 import requests
-_LOGGER = logging.getLogger(__name__)
 
 def get_tibber_token(email: str, password: str) -> str:
     """
@@ -19,11 +13,8 @@ def get_tibber_token(email: str, password: str) -> str:
     if response.status_code == 200:
         token = response.json().get('token')
         if token:
-            _LOGGER.info("Token erfolgreich abgerufen.")
             return token
         else:
-            _LOGGER.error("Token konnte nicht aus der Antwort extrahiert werden.")
             raise ValueError("Token konnte nicht aus der Antwort extrahiert werden.")
     else:
-        _LOGGER.error(f"Authentifizierung fehlgeschlagen: {response.status_code} - {response.text}")
         raise ValueError(f"Authentifizierung fehlgeschlagen: {response.status_code} - {response.text}")
