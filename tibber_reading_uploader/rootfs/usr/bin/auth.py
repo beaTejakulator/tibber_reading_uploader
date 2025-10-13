f05692d5189b3d3b2c9c9e5ccbd34c2536de3818
beaTejakulator <78610347+beaTejakulator@users.noreply.github.com>
2025-10-13 08:59:56 +0200

    Update auth.py

--- tibber_reading_uploader/rootfs/usr/bin/auth.py
+++ tibber_reading_uploader/rootfs/usr/bin/auth.py
@@ -1,35 +1,10 @@
-import requests
+# /usr/bin/auth.py
+import os
 
-def get_tibber_token(email: str, password: str) -> str:
-    """
-    Funktion, um den Token über eine API-Abfrage zu erhalten.
-    
-    Args:
-        email (str): Die E-Mail-Adresse des Benutzers.
-        password (str): Das Passwort des Benutzers.
-    
-    Returns:
-        str: Der abgerufene Token.
-    
-    Raises:
-        ValueError: Wenn die Authentifizierung fehlschlägt oder der Token nicht extrahiert werden kann.
-    """
-    auth_url = "https://app.tibber.com/v1/login.credentials"
-    auth_data = {
-        "email": email,
-        "password": password
-    }
+class MissingToken(Exception): pass
 
-    try:
-        response = requests.post(auth_url, json=auth_data)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        raise ValueError(f"Authentifizierungsanfrage fehlgeschlagen: {e}")
-
-    try:
-        token = response.json().get('token')
-        if not token:
-            raise ValueError("Token konnte nicht aus der Antwort extrahiert werden.")
-        return token
-    except (ValueError, KeyError) as e:
-        raise ValueError(f"Fehler beim Verarbeiten der Antwort: {e}")
+def get_tibber_token():
+    token = os.getenv("TIBBER_TOKEN")
+    if not token:
+        raise MissingToken("Setze die Umgebungsvariable TIBBER_TOKEN im Add-on!")
+    return token
